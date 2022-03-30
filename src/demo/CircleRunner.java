82c74e1dba1a8f5c0c6702f5d9a64c696643d08c
Zehan Zhao <cnallenzhao@gmail.com>
2022-03-30 12:12:26 -0500

    Update CircleRunner.java

--- src/demo/CircleRunner.java
+++ src/demo/CircleRunner.java
@@ -1,28 +1,46 @@
 package demo;
+
 import java.util.ArrayList;
+import java.util.Random;
+
 public class CircleRunner {
 
 	public static void main(String[] args) {
 		ArrayList<Circle> circles = new ArrayList<Circle>();
 		Circle c1 = new Circle(1.0);
 		Circle c2 = new Circle(2.0);
-		Circle c3 = new Circle(); // 0.0 
+		Circle c3 = new Circle(); // 0.0
 		circles.add(c1);
 		circles.add(c2);
 		circles.add(c3);
-		Circle circleToChange = new Circle(10000000.0);
-		int i = 0;
-		for (i = 0; i < circles.size(); ++i) {
-			System.out.println(circles.get(i));
+		System.out.println("Confirm ArrayList after add:");
+		for (Circle c : circles) {
+			System.out.println(c);
 		}
-//		for (i = 0; i < circles.size(); ++i ) {
-//			circles.set(i, circleToChange);
-//		}
-		for (Circle loop: circles) {
-		loop.setRadius(10000000.0);
-		}
+		int i;
 		for (i = 0; i < circles.size(); ++i) {
-			System.out.println(circles.get(i));
+			Circle toChanage = new Circle((double) i + 5);
+			circles.set(i, toChanage); // works, changes circles.
+		}
+		System.out.println("After regular for loop & set:");
+		for (Circle c : circles) {
+			System.out.println(c);
+		}
+		System.out.println("Retry set using enhanced for loop:");
+		for (Circle loop : circles) {
+			Circle circleToChange = new Circle(500.0);
+			loop = circleToChange; // not working!
+		}
+		for (Circle c : circles) {
+			System.out.println(c);
+		}
+		for (Circle loop : circles) {
+			double newR = new Random().nextDouble()*10;
+			loop.setRadius(newR); // works, change circles!
+		}
+		System.out.println("Confirm calling member worked:");
+		for (Circle c : circles) {
+			System.out.println(c);
 		}
 
 	}
