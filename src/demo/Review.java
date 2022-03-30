6457429d31ce6a161e79dbad126d8b42f920ae17
Zehan Zhao <cnallenzhao@gmail.com>
2022-03-30 11:49:25 -0500

    Update, save Review.java

--- src/demo/Review.java
+++ src/demo/Review.java
@@ -1,5 +1,40 @@
 package demo;
 
 public class Review {
+	private int rating;
+	private String comment;
+	
+	Review() {
+		this("No comment", -1);
+	}
+	
+	Review(String comment, int rating) {
+		this.rating = rating;
+		this.comment = comment;
+	}
+	
+	// rating setter/getter
+	public int getRating() {
+		return this.rating;
+	}
+	
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	// comment setter/getter
+	public String getComment() {
+		return this.comment;
+	}
+	
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	public void setCommentAndRating(String comment, int rating) {
+		this.comment = comment;
+		this.rating = rating;
+	}
+	
 
 }
